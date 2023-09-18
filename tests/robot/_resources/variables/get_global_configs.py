a3121ec0283596e99b82eecd0e32c041f88be5a1
vladislavploaia <vladislav.ploaia.ext@vitagroup.ag>
2023-09-18 19:59:41 +0300

    feat: add KAFKA vars for Jenkins pipeline

--- tests/robot/_resources/variables/get_global_configs.py
+++ tests/robot/_resources/variables/get_global_configs.py
@@ -1,6 +1,7 @@
 import yaml
 import os
 
+
 def get_variables():
     with open('robot/_resources/variables/additional_configs.yml', 'r') as file:
         yaml_content = yaml.safe_load(file)
@@ -14,6 +15,7 @@ def get_variables():
                 "ADMIN_BASEURL": os.environ.get('ADMIN_BASEURL', f'{ehrbase_baseurl}/ehrbase/rest/admin'),
                 "HEARTBEAT_URL": os.environ.get('HEARTBEAT_URL', f'{ehrbase_baseurl}/ehrbase/rest/status'),
                 "PLUGIN_URL": os.environ.get('PLUGIN_URL', f'{ehrbase_baseurl}/ehrbase/plugin'),
-                "RABBITMQ_URL": os.environ.get('RABBITMQ_URL', "http://127.0.0.1:15672/api")
-             }
+                "RABBITMQ_URL": os.environ.get('RABBITMQ_URL', "http://127.0.0.1:15672/api"),
+                "KAFKA_URL": os.environ.get('KAFKA_URL', "http://127.0.0.1:8082")
+            }
     return json_obj
